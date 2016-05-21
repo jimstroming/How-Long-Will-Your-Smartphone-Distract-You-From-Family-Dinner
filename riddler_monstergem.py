70f1bc17123ac113d4ffa1ba06a7f3b04935a97a
jimstroming <ajs061@motorola.com>
2016-05-21 16:38:10 -0500

    problem description

---
+++ riddler_monstergem.py
@@ -0,0 +1,16 @@
+"""
+From http://fivethirtyeight.com/features/can-you-slay-the-puzzle-of-the-monsters-gems/
+
+A video game requires you to slay monsters to collect gems. Every time you slay a monster, 
+it drops one of three types of gems: a common gem, an uncommon gem or a rare gem. 
+The probabilities of these gems being dropped are in the ratio of 3:2:1 — three common 
+gems for every two uncommon gems for every one rare gem, on average. If you slay monsters 
+until you have at least one of each of the three types of gems, how many of the most 
+common gems will you end up with, on average?
+"""
+
+"""
+
+
+
+"""
