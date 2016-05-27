6b0f415a5a1e219a643c8adc6cc0c19cd5d753e9
jimstroming <ajs061@motorola.com>
2016-05-27 08:06:38 -0500

    start this weeks problem

---
+++ riddler_baseball.py
@@ -0,0 +1,11 @@
+"""
+from http://fivethirtyeight.com/features/can-you-solve-the-puzzle-of-the-baseball-division-champs/
+
+Assume you have a sport (let’s call it “baseball”) in which each team plays 162 games in 
+a season. Assume you have a division of five teams (call it the “AL East”) where each 
+team is of exact equal ability. Specifically, each team has a 50 percent chance of 
+winning each game. What is the expected value of the number of wins for the team that
+finishes in first place?
+
+"""
+
