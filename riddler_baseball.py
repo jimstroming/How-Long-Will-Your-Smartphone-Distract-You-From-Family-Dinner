9eaf546729ae0c84b385a0ad5e7b574f548a0709
jimstroming <ajs061@motorola.com>
2016-05-28 07:44:01 -0500

    start writing the simulation routine

--- riddler_baseball.py
+++ riddler_baseball.py
@@ -1,8 +1,8 @@
 """
 from http://fivethirtyeight.com/features/can-you-solve-the-puzzle-of-the-baseball-division-champs/
 
-Assume you have a sport (let’s call it “baseball”) in which each team plays 162 games in 
-a season. Assume you have a division of five teams (call it the “AL East”) where each 
+Assume you have a sport (lets call it baseball) in which each team plays 162 games in 
+a season. Assume you have a division of five teams (call it the AL East) where each 
 team is of exact equal ability. Specifically, each team has a 50 percent chance of 
 winning each game. What is the expected value of the number of wins for the team that
 finishes in first place?
@@ -11,9 +11,32 @@ finishes in first place?
 
 """
 
-Need to make an assumption about interdivision ve intradivision games.
-Let's start with the way the schedule is doen currently:
+Need to make an assumption about interdivision vs intradivision games.
+Lets start with the way the schedule is done currently.
 19 games against each of your 4 division opponents.
 86 games outside your division.
 
 """
+
+def simulateseason(numberteams, winningpercentage, intradivisionperteam, outsidedivision):
+
+    winsperteam = []
+    
+    # play the interdivision games
+    
+    
+    # play the intradivision games
+    
+    
+    
+    return winsperteam
+
+
+numberteams = 5
+winningpercentage = .5
+intradivisionperteam = 19
+outsidedivision = 86
+
+ 
+
+print "dummy"
