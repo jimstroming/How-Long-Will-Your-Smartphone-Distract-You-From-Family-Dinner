0efc8010805bed30d495a4530cd61bc3f86e7c5d
jimstroming <ajs061@motorola.com>
2016-06-04 10:44:12 -0500

    description of the pirate riddler

---
+++ riddler_pirateelection.py
@@ -0,0 +1,23 @@
+"""
+from http://fivethirtyeight.com/features/can-you-solve-the-puzzle-of-the-pirate-booty/
+
+Ten Perfectly Rational Pirate Logicians (PRPLs) find 10 (indivisible) gold pieces and wish
+to distribute the booty among themselves.
+
+The pirates each have a unique rank, from the captain on down. The captain puts forth the 
+first plan to divide up the gold, whereupon the pirates (including the captain) vote. 
+If at least half the pirates vote for the plan, it is enacted, and the gold is distributed
+accordingly. If the plan gets fewer than half the votes, however, the captain is killed, 
+the second-in-command is promoted, and the process starts over. 
+(Theyre mutinous, these PRPLs.)
+
+Pirates always vote by the following rules, with the earliest rule taking precedence 
+in a conflict:
+
+Self-preservation: A pirate values his life above all else.
+Greed: A pirate seeks as much gold as possible.
+Bloodthirst: Failing a threat to his life or bounty, a pirate always votes to kill.
+Under this system, how do the PRPLs divide up their gold?
+
+"""
+
