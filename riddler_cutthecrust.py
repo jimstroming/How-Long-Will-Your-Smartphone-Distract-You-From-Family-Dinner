3025bcf7d4d790ee4cc2e3aeac9ec11b8a40697f
jimstroming <ajs061@motorola.com>
2016-06-17 19:56:37 -0500

    add the problem description

--- riddler_cutthecrust.py
+++ riddler_cutthecrust.py
@@ -1,11 +1,18 @@
 """
-from 
+from http://fivethirtyeight.com/features/can-you-solve-the-puzzle-of-the-picky-eater/
+
+
+Every morning, before heading to work, you make a sandwich for lunch using perfectly 
+square bread. But you hate the crust. You hate the crust so much that youll only eat 
+the portion of the sandwich that is closer to its center than to its edges so that 
+you dont run the risk of accidentally biting down on that charred, stiff perimeter. 
+How much of the sandwich will you eat?
 
 """
 
 """
 
-Let's try solving pythonically
+Let's try solving pythonically.
 
 
 """
@@ -14,7 +21,7 @@ import pdb
 
 
 def calculateclosetocrust(sidelength):
-    center = sidelength/2
+    center = float(sidelength)/2
     centercount = 0
     crustcount  = 0
     for x in range (0,sidelength):
@@ -31,8 +38,10 @@ def calculateclosetocrust(sidelength):
                 centercount += 1
             else:
                 crustcount += 1
+    print centercount
+    print crustcount
     return float(centercount) / (centercount + crustcount)
     
 print calculateclosetocrust(100)    
 print calculateclosetocrust(1000)                    
-print calculateclosetocrust(3000)                    
+print calculateclosetocrust(10000)                    
