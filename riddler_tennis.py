5e7a00ba0e2ecec6c4507b07ce039de0b848a8db
jimstroming <ajs061@motorola.com>
2016-07-03 11:03:49 -0500

    start the python solution to the tennis problem

--- riddler_tennis.py
+++ riddler_tennis.py
@@ -5,8 +5,8 @@ Your wish has been granted, and you get to play tennis against Roger Federer in 
 in the Wimbledon final. You have only a 1 percent chance to win each point, but Roger, 
 sporting gentleman that he is, offers to let you name any score and begin the match at 
 that point. (So, if youve entertained a fantasy of storming back after being down three 
-match points in the fifth set, now’s the time to live it.) What score can you name that 
-gives you the best chance to win, and what is your chance of winning the title?
+match points in the fifth set, nows the time to live it.) What score can you name that 
+gives you the best chance to win, and what is your chance of winning the title
 
 '''
 
@@ -28,11 +28,11 @@ up 5 games to love in the 3rd set, and up 40-0 in the 5th set.
 You will now have 3 match points.  Your chance of winning those
 three match points is
 
-1% + (99%)(1%) + (99%)(99%)(1%) = 2.9701 %
+.01 + (.99)(.01) + (.99)(.99)(.01) = 2.9701 percent
 
 If you lose those 3 match points, your chance of winning plummets.
 But we need to calculate to see how much the total chance
-is above 2.9701 %
+is above 2.9701 percent
 
 The analytic calculation is complicated, with the tiebreaker and 
 the 5th sent not having the tie breaker.  
@@ -40,3 +40,25 @@ Let's solve the pythonic solution.
 
 '''
 
+class TennisMatch(object):
+
+
+    setswon = [0,0]
+    gameswon = [0,0]
+    pointswon = [0,0]
+    tiebrakescore = [0,0]
+    gamepoints = ['0','15','30','40']
+    
+    def setscore(self, player1sets, player2sets, player1games, player2games, 
+                player1points, player2points):
+        self.setswon[0] = player1sets
+        self.setswon[1] = player2sets
+        self.gameswon[0] = player1games
+        self.gameswon[1] = player2games
+        self.pointswon[0] = player1points
+        self.pointswon[1] = player2points
+
+
+
+match = TennisMatch()
+match.setscore(2,0,5,0,3,0)            
