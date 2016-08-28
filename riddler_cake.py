84094d84949e7edc8d41fd5f113b4f7e58cd45a4
jimstroming <ajs061@motorola.com>
2016-08-28 16:06:46 -0500

    define the cake problem

---
+++ riddler_cake.py
@@ -0,0 +1,22 @@
+"""Can you bake the optima cake.
+From the 538 riddler
+http://fivethirtyeight.com/features/can-you-bake-the-optimal-cake/
+
+A mathematician who has a birthday coming up asks his students to 
+make him a cake. He is very particular (he is a constructive set 
+theorist, which explains a lot) and asks his students to make him a 
+three-layer cake that fits under a hollow glass cone he has on his desk. 
+(The cone was given to him as a prize for proving an obscure theorem 
+long ago.) He requires that the cake fill up the maximum amount of 
+volume under the cone as possible and that the layers of the cake 
+have straight vertical sides. (Again, the guy’s particular.) What are 
+the thicknesses of the three layers of the cake in terms of the height 
+of the glass cone? What percentage of the cone’s volume does the cake 
+fill?
+
+"""
+
+# Let's start writing some functions.
+
+import math
+
