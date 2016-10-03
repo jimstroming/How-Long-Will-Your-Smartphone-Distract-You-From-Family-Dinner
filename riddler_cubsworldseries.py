0d39106afa193de77f57a5a004b6bc5fdf321235
Jim Stroming <jimstroming@gmail.com>
2016-10-03 15:29:24 -0500

    define the cubs world series problem

---
+++ riddler_cubsworldseries.py
@@ -0,0 +1,18 @@
+""""
+from fivethirtyeight.com
+http://fivethirtyeight.com/features/cubs-world-series-puzzles-for-fun-and-profit/
+
+You are a gambler and a Cubs fan. The Cubs are competing in a seven-game series 
+against the Red Sox, first to four games wins. Your bookie agrees to take any 
+even odds bets on any of the individual games. Can you construct a series of 
+bets such that the guaranteed outcomes are You win 100 dollars if the Cubs wins 
+the series and lose 100 if the Red Sox win it?
+
+
+"""
+
+import sys
+import random
+
+
+ 
