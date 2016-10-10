1ab63a41a73ab9adde7982ba8f6ecb82bc2777e5
Jim Stroming <jimstroming@gmail.com>
2016-10-10 10:15:42 -0500

    define the 10/7 538 riddler on knight chess moves.

---
+++ riddler_knightmove.py
@@ -0,0 +1,17 @@
+"""
+How About A Nice Game Of Chess?
+From the 538 riddler
+http://fivethirtyeight.com/features/how-about-a-nice-game-of-chess/
+
+First, how long is the longest path a knight can travel on a standard 
+8-by-8 board without letting the path intersect itself?
+
+Second, there are unorthodox chess pieces that dont exist in the 
+standard game, which are known as fairy chess pieces.  
+What are the longest nonintersecting paths that can be taken by the 
+camel (which moves like a knight, except 3 squares by 1 square), 
+the zebra (3 by 2), and the giraffe (4 by 1)?
+
+"""
+
+
