f01c4859d7d173312c4711ba877a670d94f44a5e
Jim Stroming <jimstroming@gmail.com>
2016-11-19 07:14:55 -0600

    Define the party picture riddler and solve the two couple case.

---
+++ riddler_partypicture.py
@@ -0,0 +1,30 @@
+"""
+From http://fivethirtyeight.com/features/the-puzzle-of-the-lonesome-king/
+
+Five couples attend a party. They have hugged each other and said their goodbyes
+but before they leave, the 10 people arrange themselves in a straight line at
+random to take a photograph. (All possible lineups are equally likely.) What is 
+the probability that no person in the line is standing next to his or her 
+partner?
+"""
+
+
+"""
+Let's start with the simpler cases and see if there is a pattern
+
+2 couple party
+
+1 2 3 4   together
+1 2 4 3   together
+1 3 2 4
+1 3 4 2   together
+1 4 2 3
+1 4 3 2   together
+
+So, for the 2 person party
+P(together) = 4/6 = 2/3
+P(not together) = 1/3
+
+"""
+
+
