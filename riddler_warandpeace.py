dd111fd37182a2bc86774bbdc03f5d888a3ee6ff
Jim Stroming <jimstroming@gmail.com>
2016-12-13 07:37:48 -0600

    the problem description for the 12/9/16 War and Peace 538 riddler.

---
+++ riddler_warandpeace.py
@@ -0,0 +1,24 @@
+"""
+from
+http://fivethirtyeight.com/features/how-much-gold-would-push-you-into-a-war/
+
+A game theory puzzle from Juan Carrillo that indulges in international diplomacy:
+
+Consider the following war game: Two countries are eyeing each others gold. 
+At the beginning of the game, the strength of each countrys army is drawn 
+from a continuous uniform distribution and lies somewhere between 0 
+(very weak) and 1 (very strong). Each country knows its own strength but not 
+that of its opponent. The countries observe their own strength and then 
+simultaneously announce peace or war.
+
+If both announce peace, then they each stay quietly in their own territory, 
+with their own gold, which is worth $1 trillion (so each wins $1 trillion).
+
+If at least one announces war, then they go to war, and the country with the 
+stronger army wins the others gold. (That is, the stronger country wins $2 
+trillion, and the other wins $0.)
+
+What is the optimal strategy of each country (declaring peace or war) given 
+its strength?
+
+"""
