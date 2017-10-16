90c5901b1812658da9b48f9faa57d5ba26b23c62
Jim Stroming <jimstroming@gmail.com>
2017-10-15 21:29:29 -0500

    how much money can you win on jeopardy

--- riddler_jeopardy.py
+++ riddler_jeopardy.py
@@ -11,15 +11,31 @@ def simulateround(startingdollars, numbcategories, numbrows, lowdollar, highdoll
 
   # add up the total dollar amount of the board
   totaldollars =  startingdollars
+  print totaldollars
   totaldollars += numbcategories*numbrows*(highdollar + lowdollar)/2 
-  
+  print totaldollars
   # subtract out the lower dollar amount for the daily doubles, since
   # those will be the daily double, instead
   totaldollars -= numdailydoubles*lowdollar
-
+  print totaldollars
   # double the dollar amount for each daily double.
-  totaldollars = totaldollars * 2^numdailydoubles
-  
+  totaldollars = totaldollars * 2**numdailydoubles
+  print totaldollars
   return totaldollars
   
-print simulateround(0, 6, 5, 100, 500, 0)  
+  
+def simulategame():
+
+  firstroundtotal =  simulateround(0, 6, 5, 100, 500, 1)
+  print "After the first round, you can have ", firstroundtotal
+  secondroundtotal = simulateround(firstroundtotal, 6, 5, 200, 1000, 2)  
+  print "After the second round, you can have ", secondroundtotal
+  finalscore =  secondroundtotal * 2
+  print "At the end of the game, you can have",  finalscore
+  return finalscore
+  
+simulategame() 
+
+"""
+So 283,200 dollars is the most you can win.
+"""
