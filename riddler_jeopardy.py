b5f285a8fdbe2518e9fea8951a6bfe47e3d27293
Jim Stroming <jimstroming@gmail.com>
2017-10-15 20:46:56 -0500

    define the problem

---
+++ riddler_jeopardy.py
@@ -0,0 +1,8 @@
+"""
+from https://fivethirtyeight.com/features/how-much-is-a-perfect-game-of-jeopardy-worth/
+
+Austin Rodgers is having quite a run on the game show Jeopardy. As The Riddler 
+goes to press, he’s won $411,000 over 12 days. What is the maximum amount of 
+money that can possibly be won by one contestant in a single game of Jeopardy
+
+"""  
