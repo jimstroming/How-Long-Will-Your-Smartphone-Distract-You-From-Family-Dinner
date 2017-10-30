f691184a10e4ff9d50748f36d7f4d2588bba5798
Jim Stroming <jim.stroming@ibm.com>
2017-10-29 21:23:13 -0500

    Three fastest horse five thirty eight riddler.  Define the problem

---
+++ riddler_threefastest.py
@@ -0,0 +1,7 @@
+# from https://fivethirtyeight.com/features/can-you-play-the-price-is-right-perfectly/
+
+# You, a very wealthy aristocrat, own 25 horses. You are bored one day (you are bored everyday) 
+# and decide to amuse yourself by identifying the three fastest horses in your stable. 
+# However, your personal racetrack is severely lacking in capacity, and you 
+# can only race five horses at a time. What is the minimum number of races you will need 
+# to organize to identify your three fastest horses?
