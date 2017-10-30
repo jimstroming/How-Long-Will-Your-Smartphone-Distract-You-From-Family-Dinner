d5d78e45a66e53864809884ca30725e511776ccb
Jim Stroming <jim.stroming@ibm.com>
2017-10-29 21:41:00 -0500

    document the 7 race solution.

--- riddler_threefastest.py
+++ riddler_threefastest.py
@@ -9,4 +9,28 @@
 # My son suggested 11 races.  Five first round races, that elminated the bottom two in each race,
 # then six follow up races.  
 
+# To do better, let's still do the first five round races:  Let's say the results were
+
+# Race 1: 1,2,3,4,5
+# Race 2: 6,7,8,9,10
+# Race 3: 11,12,13,14,15
+# Race 4: 16,17,18,19,20
+# Race 5: 21,22,23,24,25
+
+# This eliminates horses 4,5,9,10,14,15,19,20,24,25
+
+# For race 6, let's race  the winners to find the fastest horse.  Say the results are
+
+# Race 6: 1,6,11,16,21
+
+# So, now we know that Horse 1 is the fastest horse.  6 at best is the 2nd fastest horse.  
+# That eliminates horse 8.  
+# 11 at best is the 3rd fastest horse.  That eliminated horse 12 and 13.
+# And all the horses in race 4 and 5 are eliminated.
+
+# Race 7 is now between all the horses remaining contention for 2nd and 3rd plae.
+
+# Race 7: 2, 3, 6, 7, 11
+
+# So, seven races are needed to find the three fastest.
 
